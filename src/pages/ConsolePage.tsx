24cb47f7614a172700c08dbc3ca543e32401e54a
ZS-Weng <108080619+ZS-Weng@users.noreply.github.com>
2024-11-22 13:00:46 +0800

    file updated

--- src/pages/ConsolePage.tsx
+++ src/pages/ConsolePage.tsx
@@ -377,9 +377,10 @@ export function ConsolePage() {
     const client = clientRef.current;
 
     // Set instructions
+    client.updateSession({ model: 'gpt-4o-mini' });
     client.updateSession({ instructions: instructions });
     // Set transcription, otherwise we don't get user transcriptions back
-    client.updateSession({ input_audio_transcription: { model: 'whisper-1' } });
+    client.updateSession({ input_audio_transcription: { model: 'whisper-1'} });
 
     // Add tools
     client.addTool(
